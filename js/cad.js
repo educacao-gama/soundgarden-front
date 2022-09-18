9954bd3c42105e06ffdf93265d591ca749a81f42
MailtonOliviera <mailton.s.oliveira@gmail.com>
2022-09-18 13:33:03 -0300

    ateração de cad-js

--- js/cad.js
+++ js/cad.js
@@ -1,9 +1,22 @@
-const newEvent = document.querySelector("#nome")
 
-async function cadEvent(){
+
+async function cadEvent(name){
     try{
-        const response = await postMessage("https://xp41-soundgarden-api.herokuapp.com/events");
+        const response = await fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
+            method: "POST"
+        });
+        const data = await response.json();
+       
+
+        data.onsubmit = () => {
+            const name = document.querySelector("#nome");
+            cadEvent(name.value);
+        }
+        
 
 } catch (error){
     console.log("ta dando pau " + error)
-}}
+    }
+
+}
+cadEvent()
