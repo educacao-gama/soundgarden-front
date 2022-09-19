b13e0884178ab033aecc7934d5fdb37f36263145
MailtonOliviera <mailton.s.oliveira@gmail.com>
2022-09-19 03:08:50 -0300

    alteração cad-js

--- js/cad.js
+++ js/cad.js
@@ -1,11 +1,24 @@
-const nameForm = document.querySelector(".col-6");
 
-
-async function cadEvent(body){
+async function cadEvent(){
     try{
+        const newForm = document.querySelectorAll(".col-6");
+
+        const newEvent = {
+            name: "name",
+            poster: "link da imagem",
+            attractions: [
+                "atracoes"
+            ],
+            description: "descricao",
+            scheduled: "2022-09-18T22:00:00.000Z",
+            number_tickets: 10
+        }; console.log(newEvent)
+           
+        
+        
         const response = await fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
             method: "POST",
-            body: JSON.stringify(cadEvent),
+            body: JSON.stringify(newEvent),
             Headers: { 
                 "Content-Type": "application/json; charset=UTF-8",
             },
@@ -15,7 +28,7 @@ async function cadEvent(body){
         console.log(response)
 
     
-}catch (error){
+    }catch (error){
     console.log("ta dando pau no cadastro " + error)
     };
 }
