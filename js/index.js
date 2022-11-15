5a25af481b61b9b12daf82235d61360952ed9818
tiagospeckart <tiagospeckart@gmail.com>
2022-11-15 20:29:00 -0300

    fix na data e limpeza de codigo

--- js/index.js
+++ js/index.js
@@ -1,25 +1,28 @@
-// <section> via ID
-const eventoDiv = document.querySelector("#event-list");
+// A Fazer:
+// Opcional - função para banner rotativo de eventos
+
+const eventDiv = document.querySelector("#event-list");
 
 async function getEvent() {
   const response = await fetch(
     `https://xp41-soundgarden-api.herokuapp.com/events`);
-  // A resposta é então processada pelo método .json() que pega a response
-  // e a transforma em um objeto em JS notices
-  let eventos = await response.json();
+
+  let events = await response.json();
 
   for (let i = 0; i < 3; i++){
-    let evento = eventos[i];
+    let event = events[i];
+    const date = new Date(event.scheduled);
+    const formattedDate = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear(); 
+
     const cardEvento = `
     <article class="evento card p-5 m-3">
-        <h2>${evento.name} ${evento.scheduled.slice(0, 10).split('-').join('/')}</h2>
-        <h4>${evento.attractions}</h4>
-        <p>${evento.description}</p>
+        <h2>${event.name} - ${formattedDate}</h2>
+        <h4>${event.attractions}</h4>
+        <p>${event.description}</p>
         <a href="#" class="btn btn-primary">reservar ingresso</a>
     </article>            `;
-    // HTML de section += HTML anterior + HTML do loop atual
-    // É um append
-    eventoDiv.innerHTML += cardEvento;
+
+    eventDiv.innerHTML += cardEvento;
     };
 };
 
