28dd526879ea3b465c4e0cf6ea0172853f2173e9
tiagospeckart <tiagospeckart@gmail.com>
2022-11-16 23:08:08 -0300

    Teste de implementação da postagem de reserva por meio - não funcional

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const eventDiv = document.querySelector("#event-list");
+const eventDiv = document.querySelector("#event-list"); // seletor das divs dos cards
+const reservaId = document.querySelector('.reserva'); // seletor da classe reserva no <a> (ver linha 20)
+const modalForm = document.querySelector('form'); // seletor do form do modal
 
 async function getEvent() {
   const response = await fetch(
@@ -8,49 +10,26 @@ async function getEvent() {
 
   for (let i = 0; i < 3; i++) {
     let event = events[i];
-    const date = new Date(event.scheduled);
-    const formattedDate = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+    const date = new Date(event.scheduled).toLocaleDateString();
 
     const cardEvento = `
     <article class="evento card p-5 m-3">
-        <h2>${event.name} - ${formattedDate}</h2>
+        <h2>${event.name} - ${date}</h2>
         <h4>${event.attractions}</h4>
         <p>${event.description}</p>
-        <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#cadUsuarioModal">reservar ingresso</a>
+        <a href="#" id=${event._id} class="btn btn-primary reserva" data-toggle="modal" data-target="#cadUsuarioModal">reservar ingresso</a>
     </article>            `;
 
     eventDiv.innerHTML += cardEvento;
-  };
+    };
 };
-
-getEvent();
-
-
-// TESTE
-/*
-Pseudocódigo:
-
-getEvents();
-1) no final de index.html, quando a página é carregada, a função getEvent() dentro de index.js é chamada
-2) getEvent requisita à API informações que são então estocadas dentro da constante "events"
-3) for loop de 3 iterações modifica o DOM, alterando o HTML
-4) encerramento da 
-
-Modal:
-1) Ao se clicar no evento, surge um modal com um formulário
-2) Requisição de nome e e-mail com validação no frontend ok
-3) Pergunta: como chamar uma nova requisição de API com base no ID do evento? Onde ele poderia ficar estocado?
-
-
-Código abaixo é cópia de postEvents.js
-*/
-
-const form = document.getElementById('form');
-
-async function postEvent(formData) {
+// até aqui, o mesmo código de antes
+// ---
+// função async que vai receber a formdata abaixo
+async function postModal(formData) {
   try {
     const response = await fetch(
-      'https://xp41-soundgarden-api.herokuapp.com/events',
+      'https://xp41-soundgarden-api.herokuapp.com/bookings',
       {
         method: 'POST',
         body: JSON.stringify(formData),
@@ -64,33 +43,33 @@ async function postEvent(formData) {
   } catch(error){
     console.log(error);
   }
- 
 }
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const name = event.target.name.value;
-  const attractions = event.target.attractions.value;
-  const description = event.target.description.value;
-  const scheduled = event.target.scheduled.value;
-  const tickets = event.target.tickets.value;
+// Tentativa de fazer uma função onde, ao <a> ser clicado, ele guardasse o valor de Id numa variável
+let modalId = function reservaClique {
+  let reserva = document.querySelector('.reserva');
 
-  const dataList = scheduled.split('/');
-  const day = dataList[0];
-  const month = dataList[1];
-  const yearHour = dataList[2];
-  const date = month + '/' + day + '/' + yearHour;
+  }
+
+// cópia modificada de postEvent
+modalForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const owner_name = event.target.owner_name.value;
+  const owner_email = event.target.owner_email.value;
+  const number_tickets = 1;
+  const event_id = ; // aqui é o X da questão, como por a Id aí?
 
   const formData = {
-    name,
-    attractions: attractions.split(','),
-    description,
-    scheduled: new Date(date).toISOString(),
-    number_tickets: parseInt(tickets),
-    poster: '#'
+    owner_name,
+    owner_email,
+    number_tickets,
+    event_id:
   };
 
-  postEvent(formData).then(event => {
-    window.location.href = '/admin.html';
+  postModal(formData).then(event => {
+    window.location.href = '/'; // talvez não seja necessário essas linhas de código no final, já que o modal fecha sozinho em tese
   });
 });
+
+
+getEvent();
